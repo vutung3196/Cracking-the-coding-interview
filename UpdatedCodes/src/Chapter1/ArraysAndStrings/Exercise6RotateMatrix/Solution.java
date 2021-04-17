33e2671572214a5a11975bbfeebc1e60863c33af
Tung Vu <vutung3196@outlook.com>
2021-04-17 15:08:38 +0700

    Update compress string problem

--- UpdatedCodes/src/Chapter1/ArraysAndStrings/Exercise6RotateMatrix/Solution.java
+++ UpdatedCodes/src/Chapter1/ArraysAndStrings/Exercise6RotateMatrix/Solution.java
@@ -11,24 +11,16 @@ public class Solution {
         int countCompressed = countCompression(input);
         if (countCompressed >= input.length()) return input;
 
-        int consecutiveCount = 1;
-        int index1 = 0, index2 = 1;
-        while (index1 < input.length() && index2 < input.length()) {
-            if (input.charAt(index1) == input.charAt(index2)) {
-                consecutiveCount++;
-                continue;
-            }
-            stringBuilder.append(input.charAt(index1));
-            stringBuilder.append(consecutiveCount);
-            consecutiveCount = 1;
-            index1++;
-            index2++;
-        }
-        // index 2 == input.length()
-        stringBuilder.append(input.charAt(index1));
-        stringBuilder.append(consecutiveCount);
+        int countConsecutive = 0;
+        for (int i = 0; i < input.length(); i++) {
+            countConsecutive++;
 
-        if (stringBuilder.length() >= input.length()) return input;
+            if (i + 1 >= input.length() || input.charAt(i) != input.charAt(i + 1)) {
+                stringBuilder.append(input.charAt(i));
+                stringBuilder.append(countConsecutive);
+                countConsecutive = 0;
+            }
+        }
         return stringBuilder.toString();
     }
 
@@ -46,6 +38,29 @@ public class Solution {
             }
         }
         return compressedLength;
+    }
+
+    // no need to use 2 pointers here
+    static StringBuilder compressedByUsingTwoPointers(String input) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int consecutiveCount = 1;
+        int index1 = 0, index2 = 1;
+        while (index1 < input.length() && index2 < input.length()) {
+            if (input.charAt(index1) == input.charAt(index2)) {
+                consecutiveCount++;
+                continue;
+            }
+            stringBuilder.append(input.charAt(index1));
+            stringBuilder.append(consecutiveCount);
+            consecutiveCount = 1;
+            index1++;
+            index2++;
+        }
+
+        // index 2 == input.length()
+        stringBuilder.append(input.charAt(index1));
+        stringBuilder.append(consecutiveCount);
+        return stringBuilder;
     }
 
     public static void main(String[] args) {
