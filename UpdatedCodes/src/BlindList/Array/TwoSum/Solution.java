a6ce6d17b3f3638dd6cfaaab1a2210957813638b
vutung3196 <tungvu3196@gmail.com>
2022-07-31 10:57:32 -0400

    Add library

--- UpdatedCodes/src/BlindList/Array/TwoSum/Solution.java
+++ UpdatedCodes/src/BlindList/Array/TwoSum/Solution.java
@@ -5,20 +5,20 @@ import java.util.HashMap;
 public class Solution {
     // Given an array of integers nums and an integer target,
     // return indices of the two numbers such that they add up to target.
-    public int[] twoSum(int[] nums, int target) {
-        var result = new int[2];
-        var value_to_index = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            value_to_index.put(nums[i], i);
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            var currentElement = value_to_index.get(nums[i]);
-            if (currentElement = )
-        }
-    }
+//    public int[] twoSum(int[] nums, int target) {
+//        var result = new int[2];
+//        var value_to_index = new HashMap<Integer, Integer>();
+//        for (int i = 0; i < nums.length; i++) {
+//            value_to_index.put(nums[i], i);
+//        }
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            var currentElement = value_to_index.get(nums[i]);
+//            if (currentElement = 1)
+//        }
+//    }
 
     public static void main(String[] args) {
-
+        System.out.println("Hello World");
     }
 }
