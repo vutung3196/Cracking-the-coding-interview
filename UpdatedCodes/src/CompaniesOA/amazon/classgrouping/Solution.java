3bafdb0c377a2ec206ebe45efef786e8b82af6ec
TungVu <tungvu3196@gmail.com>
2022-10-13 21:40:13 -0400

    Add Amazon problems

---
+++ UpdatedCodes/src/CompaniesOA/amazon/classgrouping/Solution.java
@@ -0,0 +1,11 @@
+package CompaniesOA.amazon.classgrouping;
+
+public class Solution {
+    // link: https://cybergeeksquad.co/2022/10/class-grouping-amazon-oa-2022-solution.html
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
---
+++ UpdatedCodes/src/CompaniesOA/amazon/findtotalimbalance/Solution.java
@@ -0,0 +1,26 @@
+package CompaniesOA.amazon.findtotalimbalance;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Solution {
+
+    // link: https://cybergeeksquad.co/2022/10/find-total-imbalance-amazon-oa-2022.html
+/**
+ *
+    Given an array of n elements find the count of imbalances in it. Imbalance is arr[i+1] - arr[i] > 1 in sorted array
+ */
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int getImbalance(int[] ranks) {
+        var result = 0;
+        Arrays.sort(ranks);
+
+
+        return result;
+    }
+}
