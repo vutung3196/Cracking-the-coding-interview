f5cad75edee23248b4f4e94d9cdad30632b0e887
Tung Vu <tungvu3196@gmail.com>
2022-11-03 20:49:06 -0400

    Add dynamic programming problems

---
+++ UpdatedCodes/src/dynamicprogramming/coinchange/Solution.java
@@ -0,0 +1,20 @@
+package dynamicprogramming.coinchange;
+
+public class Solution {
+
+    /**
+     * You are given an integer array coins representing coins of
+     * different denominations and an integer amount representing a total amount of money.
+     * Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+     *
+     * You may assume that you have an infinite number of each kind of coin.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+
+    }
+}
--- UpdatedCodes/src/dynamicprogramming/diceroll/Solution.java
+++ UpdatedCodes/src/dynamicprogramming/diceroll/Solution.java
@@ -44,4 +44,53 @@ public class Solution {
 
         return memo[diceIndex][currSum] = ways;
     }
+
+    // really difficult, I don't really understand lol!!!!
+    private static int waysToTargetBottomUp(int n) {
+        return -1;
+    }
+
+    // the number of ways to roll n dices to get sum value equals to s
+    // intuitive approach
+    private static int algoMemo(int n, int k, int s) {
+        int[][] memo = new int[n+1][s+1];
+        for (int i = 0; i < memo.length; i++) {
+            for (int j = 0; j < memo[0].length; j++) {
+                memo[i][j] = -1;
+            }
+        }
+        return numRollsToTargetSecondIdea(memo, 0, n, k, 0, s);
+    }
+
+    private static int numRollsToTargetSecondIdea(int[][] memo, int diceIndex, int n, int k, int currentSum, int targetSum) {
+        if (diceIndex == n) {
+            return currentSum == targetSum ? 1 : 0;
+        }
+
+        if (memo[diceIndex][currentSum] != -1) {
+            return memo[diceIndex][currentSum];
+        }
+
+        int ways = 0;
+        for (int x = 1; x <= k; x++) {
+            ways = ways + numRollsToTargetSecondIdea(memo, diceIndex + 1, n, k, currentSum + x, targetSum);
+        }
+
+        memo[diceIndex][currentSum] = ways;
+        return memo[diceIndex][currentSum];
+    }
+
+
+    private static int numRollsToTargetIntuitive(int n, int k, int s) {
+        if (s < 0) return 0;
+        if (n == 0) {
+            if (s == 0) return 1;
+            else return 0;
+        }
+        int result = 0;
+        for (int x = 1; x <= k; x++) {
+            result = result + numRollsToTargetIntuitive(n - 1, k, s - x);
+        }
+        return result;
+    }
 }
