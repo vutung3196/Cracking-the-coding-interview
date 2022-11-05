5c908b96b5541459a8d2c584b9e7ce9b86da9273
TungVu <tungvu3196@gmail.com>
2022-11-04 20:43:50 -0400

    Add coin change problem

--- UpdatedCodes/src/dynamicprogramming/coinchange/Solution.java
+++ UpdatedCodes/src/dynamicprogramming/coinchange/Solution.java
@@ -14,7 +14,40 @@ public class Solution {
 
     }
 
-    public static int coinChange(int[] coins, int amount) {
-
+    public static int coinChange(int[] coins, int target) {
+        // using recursion here
+        // f(i, s) = min(f(i - 1, s), f(i - 1, s - coins[i]))
+        // khong lay i thi f(i-1, s)
+        // lay i thi f(i - 1, s - coins[i])
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] <= target) {
+            }
+        }
+        return -1;
     }
+
+    public static int coinChangeBruteForce(int[] coins, int target) {
+        return coinChangeBruteForceVersion(0, coins, target);
+    }
+
+    private static int coinChangeBruteForceVersion(int idxCoin, int[] coins, int target) {
+        // coin frequencies [x0, x1,..., x(n-1)]
+        if (target == 0) return 0;
+        if (idxCoin < coins.length && target > 0) {
+            int maxNumberOfCoins = target / coins[idxCoin];
+            // initialize the minimum number of coins
+            int minimumNumberOfCoins = Integer.MAX_VALUE;
+            for (int number = 0; number < maxNumberOfCoins; number++) {
+                if (target >= number * coins[idxCoin]) {
+                    int res = coinChangeBruteForceVersion(idxCoin + 1, coins, target - number * coins[idxCoin]);
+                    if (res != -1)
+                        minimumNumberOfCoins = Math.min(minimumNumberOfCoins, res + number);
+                }
+            }
+            return (minimumNumberOfCoins == Integer.MAX_VALUE) ? -1 : minimumNumberOfCoins;
+        }
+        return -1;
+    }
+
+
 }
