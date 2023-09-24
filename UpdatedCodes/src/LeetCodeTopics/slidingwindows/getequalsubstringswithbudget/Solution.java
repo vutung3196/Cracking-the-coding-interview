f9f42f533574e97882cf5f93e2c27c4f0f309db4
Tung Vu <tungvu3196@gmail.com>
2023-09-23 22:31:34 -0400

    add some problems

---
+++ UpdatedCodes/src/LeetCodeTopics/slidingwindows/getequalsubstringswithbudget/Solution.java
@@ -0,0 +1,12 @@
+package LeetCodeTopics.slidingwindows.getequalsubstringswithbudget;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public int equalSubstring(String s, String t, int maxCost) {
+
+    }
+}
+l
--- UpdatedCodes/src/LeetCodeTopics/slidingwindows/longestsubarrayof1/Solution.java
+++ UpdatedCodes/src/LeetCodeTopics/slidingwindows/longestsubarrayof1/Solution.java
@@ -1,26 +1,32 @@
 package LeetCodeTopics.slidingwindows.longestsubarrayof1;
 
 public class Solution {
+
+    // Prev: Amazon, Apple and Yandex
     public static void main(String[] args) {
         var input = new int[]{1, 1, 0, 1};
         System.out.println(longestSubarray(input));
     }
 
+    // Time: O(n)
+    // Space: O(1)
     public static int longestSubarray(int[] nums) {
         int longestWindow = 0;
-        int zeroCount = 0;
         int start = 0;
-        for (int i = 0; i < nums.length; i++) {
-            zeroCount += (nums[i] == 0 ? 1 : 0);
+        int j = 0;
+        int N = nums.length;
+        int zeroCount = 0;
+        for (; j < N; j++) {
+            zeroCount += (nums[start] == 0 ? 1 : 0);
 
             // shrink the window until the zero counts come under the limit
             // invalid case based on template
+            // keep shrinking the window until the zero counts come under the limit
             while (zeroCount > 1) {
                 zeroCount -= (nums[start] == 0 ? 1 : 0);
                 start++;
             }
-
-            longestWindow = Math.max(longestWindow, i - start);
+            longestWindow = Math.max(longestWindow, j - start);
         }
 
         return longestWindow;
