8f92f361f5e264e628362908d1a01f623da7cd00
Tung Vu <tungvu3196@gmail.com>
2024-11-19 21:23:15 -0500

    add problems

---
+++ UpdatedCodes/src/neetcode150/slidingwindows/fixedwindowsize/maxnumberofoccurenceofsubstring/Solution.java
@@ -0,0 +1,11 @@
+package neetcode150.slidingwindows.fixedwindowsize.maxnumberofoccurenceofsubstring;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
+
+    }
+}
